8c7d222b67f2c573e6190c8e178db45c9e20ceef
Your name hereChristopher <christopher.filkins@gmail.com>
2020-02-27 09:43:12 -0700

    Removed book function, added name.

--- web_app/app.py
+++ web_app/app.py
@@ -6,7 +6,6 @@ from flask import Flask, jsonify, render_template, request
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
-from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
 from web_app.routes.stats_routes import stats_routes
@@ -25,7 +24,6 @@ def create_app():
     migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
-    app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
     app.register_blueprint(admin_routes)
     app.register_blueprint(stats_routes)
--- web_app/models.py
+++ web_app/models.py
@@ -6,11 +6,6 @@ db = SQLAlchemy()
 
 migrate = Migrate()
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128))
-    author_id = db.Column(db.String(128))
-
 class User(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     screen_name = db.Column(db.String(128), nullable=False)
